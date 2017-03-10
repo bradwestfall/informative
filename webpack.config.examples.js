e5b3e9845d6ab5b333365cb1f8fc3ca5339eed31
Brad Westfall <brad@azpixels.com>
2017-03-09 21:50:34 -0700

    moving webpack to root

---
+++ webpack.config.examples.js
@@ -0,0 +1,34 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var examplePath = process.env.EXAMPLE
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, 'examples', examplePath, 'index.js')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    alias: {
+      'src': path.resolve('./src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: 'babel-loader',
+        exclude: /node_mudles/
+      }
+    ]
+  }
+}
