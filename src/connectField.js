d2c2e84e9be25bab66e95a895c88163dfcc53f90
Ellisande <justin.dragos@gmail.com>
2017-03-14 00:05:15 -0700

    Added a HOC for wrapping fields.
    Does the exact same thing as the field wrap example (ideally), but we write and maintain the HOC instead of having the consumer do it themselves. This allows us to extend the HOC with additional functionality without forcing the change on consumers.

---
+++ src/connectField.js
@@ -0,0 +1,43 @@
+import React, {Component, PropTypes} from 'react';
+
+function connectField(name) {
+  return function(WrappedComponent){
+    return class extends Component {
+      componentWillMount() {
+        this.context.registerField(name)
+      }
+      static contextTypes = {
+        registerField: PropTypes.func,
+        getFormState: PropTypes.func,
+        setFieldState: PropTypes.func,
+      }
+      render() {
+        // Bail if name not provided
+        if (!name) throw new Error('You must provide a name to connectField. Usage connectField(name)(Component)')
+        const formState = this.context.getFormState() || {}
+        const fieldState = formState.fields[name]
+
+        // Don't render if fieldState hasn't been setup
+        if (!fieldState) return null
+
+        const input = {
+          name,
+          value: fieldState.value,
+          onChange: e => this.context.setFieldState(name, { value: e.target.value, dirty: true }),
+          onFocus: e => this.context.setFieldState(name, { visited: true, active: true }),
+          onBlur: e => this.context.setFieldState(name, { active: false })
+        }
+
+        // Is this the right syntax? Its late =P
+        // Anyhow support the style of connectField(name)(<input />)
+        if(React.isValidElement(WrappedComponent)) {
+          const newProps = Object.assign({}, {...this.props}, {input, fieldState, formState})
+          return React.clone(WrappedComponent, newProps, this.props.children)
+        }
+        return <WrappedComponent {...this.props} input={input} fieldState={fieldState} formState={formState} />;
+      }
+    }
+  }
+}
+
+export default connectField;
