809480a43db4eb2d50c53ce2d3490d8a388411e6
Brad Westfall <brad@azpixels.com>
2017-03-30 22:06:22 -0700

    changing from string refs to function refs in example

--- examples/reset-form/index.js
+++ examples/reset-form/index.js
@@ -9,6 +9,9 @@ const Input = props => {
 
 class ResetPassword extends React.Component {
 
+  // Ref to form
+  static myform
+
   constructor() {
     super()
     this.onSubmit = this.onSubmit.bind(this)
@@ -18,12 +21,12 @@ class ResetPassword extends React.Component {
     // The <Form /> component has a `resetForm` method that you can access via refs
     // This pseudo code is supposed to show how you can do an async action like resetting
     // a password, then you can reset the form if you want
-    return Promise.resolve().then(this.refs.myform.resetForm)
+    return Promise.resolve().then(this.myform.resetForm)
   }
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit} ref="myform">
+      <Form onSubmit={this.onSubmit} ref={form => {this.myform = form}}>
         <Field name="password" component={Input} type="password" /><br />
         <Field name="newPassword" component={Input} type="password" /><br />
         <button type="submit">Change Password</button>
